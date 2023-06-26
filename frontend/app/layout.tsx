083095934f7ee88ab1397cd6d0eeb9e96bed6e69
Constantin Rohde <rohdeconstantin@gmail.com>
2023-06-26 20:06:00 +0200

    added icons

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,3 +1,8 @@
+export const metadata = {
+  title: "Fichiers",
+  description: "A File Sharing Application",
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -5,6 +10,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+      </head>
       <body>{children}</body>
     </html>
   )
